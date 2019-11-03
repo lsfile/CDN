5e159127bca36e9ee00aa4cbdfad07048d99d699
Eve <57066561+lsfile@users.noreply.github.com>
2019-11-03 00:30:11 +0000

    Index Page

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	<head lang="en">
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-		<title>Cute File Browser</title>
+		<title>Database Files</title>
 		<!-- Include our stylesheet -->
 		<link href="assets/css/styles.css" rel="stylesheet"/> 
 		
